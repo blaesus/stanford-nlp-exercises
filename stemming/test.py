613870ea364721606da28ffaffc182a7311c8441
Andy Shu <andy@shux.in>
2016-12-06 15:42:45 +1100

    Add testing using real text and nltk

--- stemming/test.py
+++ stemming/test.py
@@ -1,4 +1,6 @@
 from porter import get_m, stem
+from nltk.stem.porter import PorterStemmer
+from nltk import word_tokenize
 
 print('\n~ get_m ~')
 
@@ -64,25 +66,25 @@ official_stem_test_data = {
     # 'hopeful': 'hope',
     # 'goodness': 'good',
 
-    # 'revival': 'reviv',
-    # 'allowance': 'allow',
-    # 'inference': 'infer',
-    # 'airliner': 'airlin',
-    # 'gyroscopic': 'gyroscop',
-    # 'adjustable': 'adjust',
-    # 'defensible': 'defens',
-    # 'irritant': 'irrit',
-    # 'replacement': 'replac',
-    # 'adjustment': 'adjust',
-    # 'dependent': 'depend',
-    # 'adoption': 'adopt',
-    # 'homologou': 'homolog',
-    # 'communism': 'commun',
-    # 'activate': 'activ',
-    # 'angulariti': 'angular',
-    # 'homologous': 'homolog',
-    # 'effective': 'effect',
-    # 'bowdlerize': 'bowdler',
+    'revival': 'reviv',
+    'allowance': 'allow',
+    'inference': 'infer',
+    'airliner': 'airlin',
+    'gyroscopic': 'gyroscop',
+    'adjustable': 'adjust',
+    'defensible': 'defens',
+    'irritant': 'irrit',
+    'replacement': 'replac',
+    'adjustment': 'adjust',
+    'dependent': 'depend',
+    'adoption': 'adopt',
+    'homologou': 'homolog',
+    'communism': 'commun',
+    'activate': 'activ',
+    'angulariti': 'angular',
+    'homologous': 'homolog',
+    'effective': 'effect',
+    'bowdlerize': 'bowdler',
 
     'probate': 'probat',
     'rate': 'rate',
@@ -99,4 +101,39 @@ for full_form in official_stem_test_data:
           'got', "'" + stemmed_form_calculated + "'")
     assert stemmed_form_correct == stemmed_form_calculated
 
-print('All tests passed')
+print('\nAll unit tests pass')
+
+print('\nValidating on real text against nltk implementation')
+
+nltk_stemmer = PorterStemmer()
+
+text = open('test.txt').read()
+tokens = sorted(list(set(word_tokenize(text.lower()))))
+
+agree_number = 0
+disagree_number = 0
+COLUMN_WIDTH = 12
+for token in tokens:
+    test_stem = stem(token)
+    nltk_stem = nltk_stemmer.stem(token)
+    if (token == test_stem) and (token == nltk_stem):
+        # Simple word
+        continue
+
+    if test_stem == nltk_stem:
+        agree_number += 1
+    else:
+        disagree_number += 1
+        print('Disagreement on',
+              ' ' * (COLUMN_WIDTH - len(token)),
+              token,
+              '>',
+              ' ' * (COLUMN_WIDTH - len(test_stem)),
+              test_stem,
+              '|',
+              nltk_stem,
+              )
+
+print('Agreement between this implementation and that of nltk is',
+      round(agree_number / (agree_number + disagree_number) * 100, 1), '%')
+
