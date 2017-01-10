4e297d54f2680bf75c8b422bbf6278b550b4cfe3
Andy Shu <andy@shux.in>
2017-01-10 12:58:52 +0800

    Implement tokeniser

--- ngram/ngram.py
+++ ngram/ngram.py
@@ -1,16 +1,34 @@
 from typing import Tuple, List, Dict, Callable
+from collections import Counter
 
-Vocabulary = List[str]
 Tokens = List[str]
-
+Vocabulary = List[str]
 Frequency_Table = Dict[Tuple, Dict[str, float]]
 
 
-def analyse_text(text: str) -> Tuple[Vocabulary, Tokens]:
-    pass
+def clean(text: str) -> str:
+    replace_table = (
+        (',', ' '),
+        ('.', ' '),
+        (';', ' '),
+        (':', ' '),
+        ('-', ' '),
+        ('?', ' '),
+        ('!', ' ')
+    )
+    result = text.lower()
+    for pair in replace_table:
+        result = result.replace(pair[0], pair[1])
+    return result
 
 
-def get_frequency_table(text: str) -> Frequency_Table:
+def analyse_text(text: str) -> Tuple[Tokens, Vocabulary]:
+    tokens = clean(text).split()
+    vocabulary = list(set(tokens))
+    return tokens, vocabulary
+
+
+def get_frequency_table(text: str, n: int = 3) -> Frequency_Table:
     pass
 
 
@@ -18,9 +36,11 @@ def laplace_smooth(table: Frequency_Table) -> Frequency_Table:
     pass
 
 
-def get_language_model(text: str, n: int=3) -> Callable[Tokens, float]:
+def get_language_model(text: str, n: int=3) -> Callable[[Tokens], float]:
     pass
 
 if __name__ == '__main__':
     text = open('./shakespeare.txt').read()
-    lm = get_language_model(text)
+    tokens, vocabulary = analyse_text(text)
+    print(tokens[0: 10])
+    print(vocabulary[0: 10])
